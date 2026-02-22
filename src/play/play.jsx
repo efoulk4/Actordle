3caa14128d1eb2a961a529c3bcb16d0940a2c5c9
efoulk4 <evanfoulk4@gmail.com>
2026-02-22 14:41:29 -0700

    fixed actors changing with timer and on submit

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
 import '../app.css';
 import { useNavigate } from 'react-router-dom';
-import { getTodaysActor } from '../service';
+import { getTodaysActor, formatTime } from '../service';
 
 export function Play() {
- const todaysActor = getTodaysActor();
+ const [todaysActor] = React.useState(getTodaysActor());
  const [secondsLeft, setSecondsLeft] = React.useState(90);
 
  useEffect(() => {
@@ -12,8 +12,7 @@ export function Play() {
             setSecondsLeft((prev) => {
                 if (prev <= 1) {
                     clearInterval(); //stop timer
-                    setTimerId(null); //identify expired timer
-                    return 0; //so if seconds == 0 then the timer has expired
+                    return 0; //so if secondsLeft == 0 then the timer has expired
                 }
                 return prev-1;
             })
@@ -22,12 +21,17 @@ export function Play() {
         return () => clearInterval(id);
     }, [])
 
+    function handleGuess(){
+        preventDefault();
+        
+    }
+
 
   return (
     <main>
           <p>USERNAME</p>
             <section className="playboard">
-                <h2>Time Remaining: 1:30</h2>
+                <h2>{formatTime(secondsLeft)}</h2>
                 <img src="Camera.png" height="150" width="150" alt="Film Camera" />
             </section>
             <section id="todaysactor">
@@ -51,7 +55,7 @@ export function Play() {
                     </tr>
                 </tbody>
             </table>
-            <form name="guessbox">
+            <form name="guessbox" onSubmit={handleGuess}>
             <input type="text" placeholder="Movie" />
             <button className="btn btn-primary btn-lg" type="submit">Submit Guess</button>
             </form>
