effa288d894a4209c3dc19284c70709b7444189a
efoulk4 <evanfoulk4@gmail.com>
2026-03-07 20:39:03 -0700

    init middlware

--- service/index.js
+++ service/index.js
@@ -1,2 +1,20 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
+
+
+const authCookieName = 'token';
+
+let users = [];
+let scores = [];
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
 app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
