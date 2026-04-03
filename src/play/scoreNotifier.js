81b25a2d72ccf7c88a86ff7791c4604a265e26e7
efoulk4 <evanfoulk4@gmail.com>
2026-04-02 22:22:06 -0600

    scoreNotifier finished

--- src/play/scoreNotifier.js
+++ src/play/scoreNotifier.js
@@ -6,7 +6,32 @@ class scoreEventNotifier {
         let port = window.location.port;
         const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
         this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
-        this.socket.onopen()
+        this.socket.onmessage = async (message) => {
+        try {
+        const event = JSON.parse(await msg.data.text());
+        this.processEvent(event);
+        }
+        catch {}
+        };
+        }
 
+
+
+      addHandler(handler) {
+    this.handlers.push(handler);
+  }
+
+     removeHandler(handler) {
+    this.handlers = this.handlers.filter((h) => h !== handler);
+  }
+    processEvent(event) {
+        this.updates.push(event);
+        if (this.updates.length > 5) {
+            this.updates.shift();
+        }
+        this.handlers.forEach((handler) => handler(this.updates));
     }
 }
+
+const notifier = new scoreEventNotifier();
+export { notifier };
