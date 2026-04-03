db2a2f46442bab3fe468b1d0d6e2cec7714c41ef
efoulk4 <evanfoulk4@gmail.com>
2026-04-02 21:33:42 -0600

    scorenotifier started

---
+++ src/play/scoreNotifier.js
@@ -0,0 +1,12 @@
+class scoreEventNotifier {
+    updates = [];
+    handlers = [];
+
+    constructor() {
+        let port = window.location.port;
+        const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+        this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
+        this.socket.onopen()
+
+    }
+}
